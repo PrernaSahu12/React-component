86e200eedfe1867a41f4d9f4223f1d20bf664da4
vaishalisahu02 <sahuvaishali521@gmail.com>
2025-09-06 03:55:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+// function Welcome(props){
+//   return <h1>Hello, {props.name}!</h1>
+// }
+// function app(){
+// return (
+//   <div>
+//     <Welcome name="vaishali"/>
+//   </div>
+// ) 
+// }
+// export default app
+
+
+// import { useState } from "react";
+
+// function Counter(){
+//    const [count, setCount] = useState(0)
+//    return(
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={()=>setCount(count+10)}>Increase</button>
+//       <button onClick={()=>setCount(count-1)}>Descrease</button>
+//     </div>
+//    )
+// }
+// export default Counter
+
+// function Student(props){
+//   return <div>
+//     <h2>Name: {props.name} | Age: {props.age} | Course: {props.course}</h2>
+//   </div>
+// }
+// function App(){
+//   return <div>
+//     <Student name="Vaishali" age ="23"  course = "c++"/>
+//         <Student name="Vaishu" age ="33"  course = "python"/>
+//     <Student name="Vaishalika" age ="24"  course = "java"/>
+
+//   </div>
+// }
+//  export default App
